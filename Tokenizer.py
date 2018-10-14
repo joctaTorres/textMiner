752e47d025e0b6b48f08eb7da867dfa7f678498f
jocta.torres <jocta.torres@gmail.com>
2018-10-14 20:39:24 -0300

    Saving custom tagger train with pickle

--- Tokenizer.py
+++ Tokenizer.py
@@ -1,7 +1,9 @@
 from nltk.tokenize import word_tokenize as wt
 from nltk.tokenize import PunktSentenceTokenizer as pt
 from nltk import pos_tag as ptag
-
+from nltk.corpus import mac_morpho
+import nltk
+import pickle
 import string
 
 
@@ -13,23 +15,45 @@ class Tokenizer():
 
     def tokenizeFileWords(self, path):
         return self.tokenize(
-            self.openFile(path)
+            self.preprocess(
+                self.openFile(path)
+            )
         )
 
     def openFile(self, path):
         return open(path, "r").read()
 
     def tokenize(self, text):
+        #customTagger = self.train()
+        train_file = open("trainedTagger.pickle", "rb")
+        customTagger = pickle.load(train_file)
+        train_file.close()
         punktTokenizer = pt(text)
         tSentences =  punktTokenizer.tokenize(text)
         punkt = []
         for sentence in tSentences:
             try:
                 words = wt(sentence)
-                punkt.append(ptag(words)) 
+                punkt.append(customTagger.tag(words))
             except Exception as e:
                 print('Could not tokenize sentence, reason:', str(e))
         return punkt
+
+    '''def train(self):
+        tsents = mac_morpho.tagged_sents()
+        tsents = [[(w.lower(), self.simplify_tag(t)) for (w,t) in sent] for sent in tsents if sent]
+        train = tsents[100:]
+        test = tsents[:100]
+        tagger0 = nltk.DefaultTagger('n')
+        tagger1 = nltk.UnigramTagger(train, backoff=tagger0)
+        tagger2 = nltk.BigramTagger(train, backoff=tagger1)        
+        return nltk.TrigramTagger(train, backoff=tagger2)
+
+    def simplify_tag(self, t):
+        if "+" in t:
+            return t[t.index("+")+1:]
+        else:
+            return t'''
 
     def preprocess(self, text):
         punctuations = r'''!()-[]{};:'"’\,<>./?@#$%^&*_~'''
@@ -38,6 +62,7 @@ class Tokenizer():
             if char not in punctuations:
                no_punct = no_punct + char
         return no_punct
+
 
 
 words = Tokenizer('portuguese').tokenizeFileWords('./example.txt')
