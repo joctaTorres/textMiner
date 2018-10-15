bbd9030c76da52d34727ec8892c82fc26d9ad1c4
jocta.torres <jocta.torres@gmail.com>
2018-10-14 23:30:23 -0300

    Writting Miner.py

--- Miner.py
+++ Miner.py
@@ -1,10 +1,30 @@
 from Tokenizer import Tokenizer
 from TokenWritter import TokenWritter
 
+class Miner():
+    filePath = ''
+    searchTag = []
+    searchWord = []
+    tokenizer = Tokenizer('portuguese')
+    writter = TokenWritter()
 
-tokenizer = Tokenizer('portuguese')
+    def __init__(self, path, tag, word):
+        if (path == ''):
+            raise ValueError('Path for file cannot be empty.')
+        if (tag is [] and word is []):
+            raise ValueError('You must search for a tag or a word, or both.')
 
-TokenWritter().outputTokens(
-    tokenizer.tokenizeFileWords('./example.txt'),
-    'outputtest.csv'
-    )
+        self.filePath = path
+        self.searchTag = tag
+        self.searchWord = word
+
+    def search(self):
+        pass
+
+    def generateMidFile(self):
+        self.writter.outputTokens(
+            self.tokenizer.tokenizeFileWords(self.filePath),
+            'minerMid.csv'
+        )
+
+Miner('./example.txt', ['tag'], ['word']).generateMidFile()
