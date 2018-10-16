3e390994b7956917a836d1abc233d095fdb65f33
jocta.torres <jocta.torres@gmail.com>
2018-10-15 23:42:56 -0300

    Adding basic minercart

---
+++ Minecart.py
@@ -0,0 +1,30 @@
+from Miner import Miner
+
+class Minecart():
+    miner = None
+
+    def menu(self):
+        print('1- Load file')
+        self.push()
+        print('2- Search in file')
+        self.beat()
+
+
+    def push(self):
+        file = input('Enter file name or path: ')
+        miner = Miner(file)
+    
+    def beat(self):
+        typedwords = input('Enter list of words: ')
+        typedtags = input('Enter list of tags: ')
+        listwords = typedwords.split()
+        listtags = typedtags.split()
+    
+        df = self.miner.search(
+            tags=listtags,
+            words=listwords
+            )
+        
+        print(df)
+
+Minecart().menu()
